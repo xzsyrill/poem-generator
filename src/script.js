7a2fcedab14eb517ce6e35f2efcc73177a540dfe
xzsyrill <151985904+xzsyrill@users.noreply.github.com>
2025-07-01 16:35:54 +0800

    Revise the prompt

--- src/script.js
+++ src/script.js
@@ -1,4 +1,7 @@
 function displayPoem(response) {
+  let poem = document.querySelector("#poem");
+  poem.innerHTML = "";
+
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -13,7 +16,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "257842e3dd87eab0cfobda43203bt81d";
   let context =
-    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />, Directly and Clean. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate an English poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
