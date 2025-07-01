af0d46d5e0bbe11badffefff54e270563ea5d6f9
xzsyrill <151985904+xzsyrill@users.noreply.github.com>
2025-07-01 16:49:46 +0800

    Finalize UX

--- src/script.js
+++ src/script.js
@@ -20,6 +20,10 @@ function generatePoem(event) {
   let prompt = `User instructions: Generate an English poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
+
   axios.get(apiUrl).then(displayPoem);
 }
 
